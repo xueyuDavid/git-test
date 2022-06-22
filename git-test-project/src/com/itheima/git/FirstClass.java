590d788131020286d1d396a725775460f2b24644
glw <glw@qq.com>
2022-06-22 15:29:59 +0800

    把本地工程添加到git

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -11,4 +11,10 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
 }
